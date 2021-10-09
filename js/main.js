bb0115260b32100cc24683ce1e0bf883b8dcbfdf
Joseph Olabisi <josepholabisi6000@gmail.com>
2021-10-09 21:30:55 +0100

    Change logic of scrollTrigger

--- js/main.js
+++ js/main.js
@@ -11,21 +11,21 @@ const init = () => {
   };
 
   const progressMap = {
-    0: '0%',
-    0.2: '17%',
-    0.4: '34%',
-    0.6: '51%',
-    0.8: '68%',
-    1: '85%',
+    0: "0%",
+    0.2: "17%",
+    0.4: "34%",
+    0.6: "51%",
+    0.8: "68%",
+    1: "85%",
   };
 
   const dots = document.querySelectorAll(".dot");
-  const progressEl = document.querySelector('.footer-progress');
+  const progressEl = document.querySelector(".footer-progress");
+  const sections = document.querySelectorAll(".sections");
 
   let currentActiveDot = 0;
 
-
-  //Jump to a sectin when each dot is clicked
+  //Jump to a section when each dot is clicked
   dots.forEach((dot) => {
     dot.addEventListener("click", () => {
       window.scrollTo({
@@ -35,6 +35,16 @@ const init = () => {
       });
     });
   });
+
+  // sections.forEach((section, index) => {
+  //   gsap.to(section, {
+  //     scrollTrigger: {
+  //       trigger: section,
+  //       onEnter: () => setActiveDot(index),
+  //       onLeave: () => removeActiveDot(index),
+  //     },
+  //   });
+  // });
 
   gsap.from(".section-1 .section-image .image-props img", {
     height: 0,
@@ -46,39 +56,49 @@ const init = () => {
 
   const timeline = gsap.timeline();
 
-  timeline.fromTo(".lines", { x: "0%" }, { x: "-40%", ease: "bounce.out" });
+  const scrollAnim = timeline
+    .to(".lines", { x: '-40%', ease: "none" }, 0)
+    .to(".container", { x: `${-100 * (sections.length - 1)}vw`, ease: "none" }, 0);
 
-  const scrollTrigger = ScrollTrigger.create({
+  ScrollTrigger.create({
     trigger: ".container",
-    animation: timeline,
+    animation: scrollAnim,
     pin: true,
-    start: "top top",
-    end: "bottom bottom",
-    scrub: 0.1,
+    // start: "top top",
+    end: `bottom+=600% bottom`,
+    scrub: 2,
     markers: true,
     pinSpacing: false,
     anticipatePin: 1,
-    snap: 1 / (6 - 1),
-    onUpdate: (update) => {
-      if (update.progress in activeMap) {
-        setActiveDot(update.progress);
-        setProgress(update.progress);
-      }
-    },
+    // snap: 1 / (sections.length - 1),
+    // onUpdate: (update) => {
+    //   if (update.progress in activeMap) {
+    //     setActiveDot(update.progress);
+    //     setProgress(update.progress);
+    //   }
+    // },
   });
 
-  const setActiveDot = (progress) => {
+  const setActiveDot = (index) => {
     //remove active class from current dot
-    dots[currentActiveDot].classList.remove("active");
+    // dots[currentActiveDot].classList.remove("active");
+    // dots[index].classList.remove("active");
+
     //update active dot
-    currentActiveDot = activeMap[progress];
+    // currentActiveDot = activeMap[progress];
+
     //add active class to updated current dot
-    dots[currentActiveDot].classList.add("active");
+    // dots[currentActiveDot].classList.add("active");
+    dots[index].classList.add("active");
+  };
+
+  const removeActiveDot = (index) => {
+    dots[index].classList.remove("active");
   };
 
   const setProgress = (progress) => {
     progressEl.style.width = progressMap[progress];
-  }
+  };
 };
 
 initGsap = init();
