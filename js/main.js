f9d0f25f92d30f7e642cecff2ed5df27b75afa05
Joseph Olabisi <josepholabisi6000@gmail.com>
2021-10-16 13:43:27 +0100

    Fix snap

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
 gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(ScrollToPlugin);
 
+let lastTime = 0;
+
+
+
+function throttle(func, timeFrame) {
+  return function () {
+      var now = Date.now();
+      if (now - lastTime >= timeFrame) {
+          func();
+          lastTime = now;
+      }
+  };
+}
+
 const init = () => {
   // const setActiveSection = (index) => {
   //   //add active class to updated current dot
@@ -22,121 +36,81 @@ const init = () => {
   // };
 
   const dots = document.querySelectorAll(".dot");
-  // const sections = document.querySelectorAll(".sections");
+  const sections = document.querySelectorAll(".sections");
 
-  // const container = document.querySelector(".container");
+  let currentSection = 0;
+
+  let lastScrollTop = 0;
+
+  window.addEventListener("wheel", (e) => {
+    const callback = () => {
+    let delta = e.deltaY;
+
+    if (delta > 0) {
+      if (currentSection < 6) {
+        currentSection += 1;
+        jumpToSection(currentSection);
+      }
+    } else {
+      if (currentSection > 0) {
+        currentSection -= 1;
+        jumpToSection(currentSection);
+      }
+    }
+  
+  
+      }
+      let t = throttle(callback, 1500);
+      t();
+    }  );
+
+  const disableScroll = () => {
+    // Get the current page scroll position
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    const scrollLeft =
+      window.pageXOffset || document.documentElement.scrollLeft;
+
+    // if any scroll is attempted, set this to the previous value
+    window.onscroll = function () {
+      window.scrollTo(scrollLeft, scrollTop);
+    };
+  };
+
+  disableScroll();
+
+  const enableScroll = () => {
+    window.onscroll = () => {};
+  };
 
   const timeline = gsap.timeline();
 
-  // let scrollTween;
+  const jumpToSection = (index) => {
+    gsap.to(window, {
+      duration: 2.5,
+      scrollTo: {
+        y: 100 * index,
+      },
+      ease: "elastic.out(1.75, 0.3)",
+      onStart: () => {
+        enableScroll();
+        setTimeout(() => {
 
-  // const jumpToSection = (index) => {
-  //   console.log("I was called", index);
-  //   scrollTween = gsap.to(window, {
-  //     // scrollTo: { x: index * innerWidth, autoKill: false },
-  //     scrollTo: { x: -100 },
-  //     duration: 1,
-  //     onComplete: () => (scrollTween = null),
-  //     overwrite: true,
-  //   });
-  // };
+          disableScroll();
+        }, 800);
+      },
+    });
+  };
 
-  const scrollAnim = timeline
-    .to(".lines", { x: "-40%", ease: "none" }, 0)
+  const scrollAnim = timeline.to(".lines", { x: "-40%", ease: "none" }, 0);
 
   //Jump to a section when each dot is clicked
   dots.forEach((dot, index) => {
     dot.addEventListener("click", (e) => {
-      console.log("i got here", e.target.getAttribute("href"));
-      console.log('width', innerWidth * (index / 2))
       e.preventDefault();
-      gsap.to(window, {
-        duration: 2.5,
-        scrollTo: {
-          y: 100 * index,
-          autoKill: true,
-        },
-        ease: 'elastic.out(1.75, 0.3)'
-      });
+      currentSection = index;
+      jumpToSection(index);
     });
   });
-
-  // console.log('sectiln', sections[0].querySelector('h1'))
-  // gsap.set(sections[0].querySelector('h1'), {yPercent: 0});
-  // gsap.set(sections[0].querySelector('p'), {yPercent: 0, opacity: 1});
-  // gsap.set(sections[0].querySelectorAll('img'), {height: 160, scale: 1});
-  // sections.forEach((section, index) => {
-  //   gsap.timeline({
-  //     scrollTrigger: {
-  //       trigger: section,
-  //       scrub: true,
-  //       // onEnter: () => {
-  //       //   setActiveSection(index);
-  //       //   setActiveSection(index);
-  //       //   setProgress(index);
-  //       // },
-  //       // onEnterBack: () => {
-  //       //   setActiveSection(index);
-  //       //   setActiveSection(index);
-  //       //   setProgress(index);
-  //       // },
-  //       // containerAnimation: scrollAnim,
-  //       // toggleActions: "play reverse play play",
-  //       // start: "center bottom",
-  //       // end: "center center-=100px",
-  //       markers: true,
-  //       onToggle: (self) => {
-  //         console.log("toggle", index, self.isActive);
-  //         if (self.isActive) {
-  //           setActiveSection(index);
-  //           setProgress(index);
-  //           // if (!scrollTween) {
-  //           //   jumpToSection(index);
-  //           // }
-  //         } else {
-  //           removeActiveSection(index);
-  //         }
-  //       },
-  //     },
-  //   });
-  //   // .fromTo(
-  //   //   section.querySelectorAll("h1"),
-  //   //   { yPercent: 100 },
-  //   //   {
-  //   //     yPercent: 0,
-  //   //     duration: 0.5,
-  //   //   },
-  //   //   0
-  //   // )
-  //   // .fromTo(
-  //   //   section.querySelector("p"),
-  //   //   { opacity: 0 },
-  //   //   { duration: 0.1, opacity: 1 },
-
-  //   // )
-  //   // .fromTo(
-  //   //   section.querySelector("p"),
-  //   //   { yPercent: 100, },
-  //   //   { yPercent: -10, duration: 0.1 }
-  //   // );
-
-  //   if (index === 0) {
-  //     gsap.to(".section-1 .section-image .image-props img", {
-  //       height: 160,
-  //       scale: 1,
-  //       duration: 1.5,
-  //       stagger: 0.1,
-  //       ease: "elastic.inOut(1, 0.4)",
-  //       scrollTrigger: {
-  //         trigger: section,
-  //         toggleActions: "play reverse play play",
-  //         containerAnimation: scrollAnim,
-  //         start: "top-=10px end",
-  //         end: "bottom center+=400px",
-  //       },
-  //     });
-  //   }
-  // });
 
   ScrollTrigger.create({
     trigger: ".container",
---
+++ js/throttle.js
@@ -0,0 +1,10 @@
+function throttle(func, timeFrame) {
+  var lastTime = 0;
+  return function () {
+      var now = new Date();
+      if (now - lastTime >= timeFrame) {
+          func();
+          lastTime = now;
+      }
+  };
+}
