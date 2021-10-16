3f53f0e2c5241da87ebab3d717e4f8059afd7a28
Joseph Olabisi <josepholabisi6000@gmail.com>
2021-10-16 19:06:19 +0100

    Text transitions complete

--- js/main.js
+++ js/main.js
@@ -1,28 +1,109 @@
 gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(ScrollToPlugin);
+gsap.registerPlugin(CSSRulePlugin);
 
 let lastTime = 0;
 
-
-
 function throttle(func, timeFrame) {
   return function () {
-      var now = Date.now();
-      if (now - lastTime >= timeFrame) {
-          func();
-          lastTime = now;
-      }
+    var now = Date.now();
+    if (now - lastTime >= timeFrame) {
+      func();
+      lastTime = now;
+    }
   };
 }
 
 const init = () => {
+  const sections = document.querySelectorAll(".sections");
+
+  let currentSection = 0;
+  let prevSection = 0;
+
+  gsap.set(sections[currentSection], {
+    opacity: 1,
+  });
+
+  gsap.set(sections[currentSection].querySelectorAll("h1"), {
+    y: 0,
+  });
+  gsap.set(sections[currentSection].querySelector("p"), {
+    y: 0,
+    opacity: 1,
+  });
   const setActiveSection = (index) => {
+    //TODO: Make timeline of tweens
     //add active class to updated current dot
-    dots[prevSection].classList.remove('active')
+    const timeline = gsap.timeline({
+    });
+
+    dots[prevSection].classList.remove("active");
     dots[index].classList.add("active");
-    prevSection = currentSection;
+
+    timeline
+      .to(
+        sections[prevSection].querySelectorAll("h1"),
+        {
+          y: 100,
+          duration: 0.1
+        },
+        0
+      )
+      .to(
+        sections[prevSection].querySelector("p"),
+        {
+          y: 100,
+          opacity: 0,
+          duration: 0.1
+        },
+        0
+      )
+      
+      .to(sections[prevSection], {
+        opacity: 0,
+        duration: 0.1
+      }, 0)
+      .to(sections[currentSection], {
+        opacity: 1,
+        duration: 0.1
+      }, '>+0.5')
+      .to(
+        sections[currentSection].querySelectorAll("h1"),
+
+        {
+          y: 0,
+          duration: 0.1
+        }, '>'
+      )
+      .to(sections[currentSection].querySelector("p"), {
+        y: 0,
+        duration: 0.1,
+      }, '>').to(sections[currentSection].querySelector("p"), {
+        opacity: 1,
+        duration: 0.1,
+        onComplete: () => {
+          prevSection = currentSection;
+        }
+      }, '>-0.1');
+
+    // sections[prevSection].classList.remove('active');
     // sections[index].classList.add("active");
   };
+
+  gsap.to(".section-1 .section-image .image-props img", {
+    height: 160,
+    scale: 1,
+    duration: 1.5,
+    stagger: 0.1,
+    ease: "elastic.inOut(1, 0.4)",
+    scrollTrigger: {
+      trigger: ".section-1",
+      toggleActions: "play reverse play play",
+      // containerAnimation: scrollAnim,
+      start: "top-=50px top",
+      end: "bottom bottom",
+    },
+  });
 
   // const removeActiveSection = (index) => {
   //   //remove active class from current dot
@@ -38,34 +119,28 @@ const init = () => {
   };
 
   const dots = document.querySelectorAll(".dot");
-  const sections = document.querySelectorAll(".sections");
-
-  let currentSection = 0;
-  let prevSection = 0;
 
   let lastScrollTop = 0;
 
   window.addEventListener("wheel", (e) => {
     const callback = () => {
-    let delta = e.deltaY;
+      let delta = e.deltaY;
 
-    if (delta > 0) {
-      if (currentSection < 6) {
-        currentSection += 1;
-        jumpToSection(currentSection);
+      if (delta > 0) {
+        if (currentSection < 6) {
+          currentSection += 1;
+          jumpToSection(currentSection);
+        }
+      } else {
+        if (currentSection > 0) {
+          currentSection -= 1;
+          jumpToSection(currentSection);
+        }
       }
-    } else {
-      if (currentSection > 0) {
-        currentSection -= 1;
-        jumpToSection(currentSection);
-      }
-    }
-  
-  
-      }
-      let t = throttle(callback, 1500);
-      t();
-    }  );
+    };
+    let t = throttle(callback, 1500);
+    t();
+  });
 
   const disableScroll = () => {
     // Get the current page scroll position
@@ -97,7 +172,6 @@ const init = () => {
       onStart: () => {
         enableScroll();
         setTimeout(() => {
-
           disableScroll();
           setActiveSection(currentSection);
           setProgress(currentSection);
